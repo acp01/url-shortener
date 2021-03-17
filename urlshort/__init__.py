7e17efaffe68bff8c11d5e4a3dff6b923c749678
Akshay Patel <acp@somaiya.edu>
2021-03-17 10:41:38 +0530

    Initial commit

---
+++ urlshort/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.secret_key = 'h456urtri76tvyytr76rt'
+
+    from . import urlshort
+    app.register_blueprint(urlshort.bp)
+
+    return app
+
+
+
